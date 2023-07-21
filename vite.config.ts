f1a199fcb3c44f7a09a6de032e02e933bc0aeff3
Gadjimurad(krutoy) <gm_061@bk.ru>
2023-07-21 17:18:38 +0300

    Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,17 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@entities": path.resolve(__dirname, "./src/entities"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@layouts": path.resolve(__dirname, "./src/layouts"),
-      "@types": path.resolve(__dirname, "./src/types"),
-      "@router": path.resolve(__dirname, "./src/router"),
+    plugins: [react()],
+    base: "./",
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@entities": path.resolve(__dirname, "./src/entities"),
+            "@pages": path.resolve(__dirname, "./src/pages"),
+            "@layouts": path.resolve(__dirname, "./src/layouts"),
+            "@types": path.resolve(__dirname, "./src/types"),
+            "@router": path.resolve(__dirname, "./src/router"),
+        },
     },
-  },
 });
