e156fe1d6eae11adc219d8b04b6e5cb38ad27fde
Gadjimurad(krutoy) <gm_061@bk.ru>
2023-07-21 18:16:51 +0300

    Add little responsiveness

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -5,7 +5,6 @@ const Footer = lazy(() => import("@/components/composition/Footer"));
 const Header = lazy(() => import("@/components/composition/Header"));
 
 const DefaultLayout: FunctionComponent<Children> = ({ children }) => {
-    console.log("message");
     return (
         <div className="h-full w-full xl:px-36 lg:px-20 pt-6 px-14 overflow-x-hidden">
             <Suspense fallback={""}>
